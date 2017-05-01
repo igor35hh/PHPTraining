dc1071dcfafbc9aaa21f7d7179cb0861dc9436c1
Igor <igor35hh@gmail.com>
2017-05-01 18:51:05 +0300

    module updates

--- PrestaShop/ModuleDevelopment/the_comments/the_comments.php
+++ PrestaShop/ModuleDevelopment/the_comments/the_comments.php
@@ -5,7 +5,7 @@
 		public function __construct() {
 			$this->name = 'the_comments';
 			$this->tab = 'front_office_features';
-			$this->version = '0.1';
+			$this->version = '0.2';
 			$this->author = 'Igor Klimets';
 
 			$this->bootstrap = true;
@@ -19,21 +19,30 @@
 
 		public function install() {
 
-			if (!parent::install() || !$this->registerHook('displayProductTabContent'))
-				return false;
-
 			//parent::install();
 			//$this->registerHook('displayProductTabContent');
 			//return true;
 
-			return Db::getInstance()->execute('
-				CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'mymod_comment` (
-					id_mymod_comment int(11) NOT NULL AUTO_INCREMENT,
-					id_product int(11) NOT NULL,
-					grade tinyint(1) NOT NULL,
-					comment text NOT NULL,date_add datetime NOT NULL,
-					PRIMARY KEY (id_mymod_comment)) ENGINE='._MYSQL_ENGINE_.'
-					DEFAULT CHARSET=utf8 AUTO_INCREMENT=1');
+			if (!parent::install() || !$this->registerHook('displayProductTabContent') || !$this->registerHook('displayBackOfficeHeader'))
+				return false;
+
+			$sql_file = dirname(__FILE__).'/install/install.sql';
+			if(!$this->loadSQLFile($sql_file))
+				return false;
+
+			Configuration::updateValue('MYMOD_GRADES', '1');
+			Configuration::updateValue('MYMOD_COMMENTS', '1');
+
+			return true;
+
+			// return Db::getInstance()->execute('
+			// 	CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'mymod_comment` (
+			// 		id_mymod_comment int(11) NOT NULL AUTO_INCREMENT,
+			// 		id_product int(11) NOT NULL,
+			// 		grade tinyint(1) NOT NULL,
+			// 		comment text NOT NULL,date_add datetime NOT NULL,
+			// 		PRIMARY KEY (id_mymod_comment)) ENGINE='._MYSQL_ENGINE_.'
+			// 		DEFAULT CHARSET=utf8 AUTO_INCREMENT=1');
 
 		}
 
@@ -41,16 +50,65 @@
 			if (!parent::uninstall())
 				return false;
 
-			return (Db::getInstance()->execute('DROP TABLE `'._DB_PREFIX_.'mymod_comment`'));
+			$sql_file = dirname(__FILE__).'/install/uninstall.sql';
+			if(!$this->loadSQLFile($sql_file))
+				return false;
+
+			Configuration::deleteByName('MYMOD_GRADES');
+			Configuration::deleteByName('MYMOD_COMMENTS');
+
+			return true;
+
+			//return (Db::getInstance()->execute('DROP TABLE `'._DB_PREFIX_.'mymod_comment`'));
+		}
+
+		public function loadSQLFile($sql_file) {
+			$sql_content = file_get_contents($sql_file);
+			$sql_content = str_replace('PREFIX_', _DB_PREFIX_, $sql_content);
+			$sql_requests = preg_split("/;\s*[\r\n]+/", $sql_content);
+
+			$result = true;
+			foreach ($sql_requests as $request) {
+				if(!empty($request))
+					$result &= DB::getInstance()->execute(trim($request));
+			}
+			return $result;
+		}
+
+		public function onClickOption($type, $href = false) {
+			$confirm_reset = $this->l('Reseting this module will delete all comments from database, are you sure?');
+			$reset_callback = "return mymodcomments_reset('".addcslashes($confirm_reset)."');";
+			$matchType = array(
+				'reset' => $reset_callback,
+				'delete' => "return confirm('Confirm delete?')",
+			);
+			if(isset($matchType[$type])) {
+				return $matchType[$type];
+			}
+			return '';
+		}
+
+		public function hookDisplayBackOfficeHeader($params) {
+			if(Tools::getValue('controller') != 'AdminModules') {
+				return '';
+			}
+			$this->context->smarty->assign('pc_base_dir', __PS_BASE_URI__.'modules/'.$this->name.'/');
+			return $this->display(__FILE__,'displayBackOfficeHeader.tpl');
 		}
 
 		public function processProductTabContent() {
 			if(Tools::isSubmit('mymod_pc_submit_comment')) {
 				$id_product = Tools::getValue('id_product');
+				$firstname = Tools::getValue('firstname');
+				$lastname = Tools::getValue('lasttname');
+				$email = Tools::getValue('email');
 			 	$grade = Tools::getValue('grade');
 			 	$comment = Tools::getValue('comment');
 			 	$insert = array (
 			 		'id_product' => (int)$id_product,
+			 		'firstname' => pSQL($firstname),
+			 		'lastname' => pSQL($lastname),
+			 		'email' => pSQL($email),
 			 		'grade' => (int)$grade,
 			 		'comment' => pSQL($comment),
 			 		'date_add' => date('Y-m-d H:i:s'),
--- PrestaShop/ModuleDevelopment/the_comments/views/templates/hook/displayProductTabContent.tpl
+++ PrestaShop/ModuleDevelopment/the_comments/views/templates/hook/displayProductTabContent.tpl
@@ -4,6 +4,8 @@
 <div class="rte">
 	{foreach from=$comments item=comment}
 		<div class="mymodcomments-comment">
+			<img src="http://www.gravatar.com/avatar/{$comment.email|trim|strlower|md5}?s=45" class="pull-left-img-thumbnail mymodcomments-avatar" />
+			<p>{$comment.firstname} {$comment.lastname|substr:0:1}</p>
 			<div class="star-rating">
 				<i class="glyphicon glyphicon-star"></i>
 				<strong>Grade:</strong> {$comment.grade}/5<br>	
@@ -21,32 +23,55 @@
 {if $enable_grades eq 1 OR $enable_comments eq 1}
 <div class="rte">
 	<form action="" method="POST" id="comment-form">
+
+			<div class="form-group">
+				<label for="firstname">{l s='Firstname:' mod='mymodcomments'}</label>
+				<div class="row">
+					<div class="col-xs-4">
+						<input type="text" name="firstname" id="firstname" class="form-control" />
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="lastname">{l s='Lastname:' mod='mymodcomments'}</label>
+				<div class="row">
+					<div class="col-xs-4">
+						<input type="text" name="lastname" id="lastname" class="form-control" />
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="email">{l s='Email:' mod='mymodcomments'}</label>
+				<div class="row">
+					<div class="col-xs-4">
+						<input type="email" name="email" id="email" class="form-control" />
+					</div>
+				</div>
+			</div>
+
 		{if $enable_grades eq 1}
 			<div class="form-group">
 				<label for="grade">Grade:</label>
 				<div class="row">
-					<div class="col-xs-4">
-						<select id="grade" class="form-control" name="grade">
-							<option value="0">-- Choose --</option>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-						</select>
+					<div class="col-xs-4" id="grade-tab">
+						<input id="grade" type="number" value="0" class="rating" name="grade" min="0" max="5" step="1" data-size="sm">
 					</div>
 				</div>
 			</div>
 		{/if}
+
 		{if $enable_comments eq 1}
 			<div class="form-group">
-				<label for="comment">Comment:</label>
+				<label for="comment">{l s='Comment:' mod='mymodcomments'}</label>
 				<textarea name="comment" id="comment" class="form-control"></textarea>
 			</div>
 		{/if}
+
 		<div class="submit">
 			<button type="submit" name="mymod_pc_submit_comment" class="button btn btn-default button-medium">
-				<span>Send<i class="icon-chevron-right right"></i></span>	
+				<span>{l s='Send' mod='mymodcomments'}<i class="icon-chevron-right right"></i></span>	
 			</button>
 		</div>
 		</form>
